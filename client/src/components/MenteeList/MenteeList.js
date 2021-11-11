28d64c8443e8cae7c8c21066371d52c345e8e057
Natalie Pilling <natalie.s.pilling@gmail.com>
2021-11-11 12:10:26 +0100

    feat: display table of all mentees

--- client/src/components/MenteeList/MenteeList.js
+++ client/src/components/MenteeList/MenteeList.js
@@ -2,10 +2,83 @@ import MenteeListItem from '../MenteeListItem/MenteeListItem';
 
 function MenteeList({ mentees }) {
   return (
-    <div>
-      {mentees.map((mentee) => (
-        <MenteeListItem key={mentee._id} mentee={mentee} />
-      ))}
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Career path
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Town
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Age
+                  </th>
+                  <th scope="col" className="relative px-6 py-3">
+                    <span className="sr-only">View more</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {/* TODO: export table row to a separate component */}
+                {/* TODO: choose which fields to show & how, then adjust table headers */}
+                {mentees.map((mentee) => (
+                  <tr key={mentee._id}>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm font-medium text-gray-900">
+                        {mentee.name}
+                      </div>
+                      <div className="text-sm text-gray-500">
+                        {mentee.careerPath}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">
+                        {mentee.careerPath}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{mentee.town}</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                        {mentee.age}
+                      </span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                      <a
+                        href="/mentee"
+                        className="text-indigo-600 hover:text-indigo-900"
+                      >
+                        View more
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
