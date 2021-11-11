61a1d84c1a377b32923b56bb796c233f92615134
Natalie Pilling <natalie.s.pilling@gmail.com>
2021-11-11 11:29:00 +0100

    feat: display navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,104 @@
+// import { Fragment } from 'react';
+import { Disclosure } from '@headlessui/react'; //Menu, Transition
+import { MenuIcon, XIcon } from '@heroicons/react/outline'; //BellIcon
+
+const navigationTabs = [
+  { name: 'Home', href: '/', current: true },
+  { name: 'My mentees', href: '/mymentees', current: false },
+];
+
+// Component from Tailwind UI https://tailwindui.com/components/application-ui/navigationTabs/navbars
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
 function Navbar() {
-  return <h1 className="">I'm a navbar</h1>;
+  return (
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              {/* Mobile menu buttons*/}
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+
+              {/* Logo section */}
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0 flex items-center">
+                  <img
+                    className="block h-4 w-auto"
+                    src="./assets/logo-white.png"
+                    alt="Dare IT logo"
+                  />
+                </div>
+
+                {/* Tab section */}
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">
+                    {navigationTabs.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? 'bg-gray-900 text-white'
+                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'px-3 py-2 rounded-md text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+
+              {/* Profile picture */}
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <img
+                  className="h-8 w-8 rounded-full"
+                  src="./assets/profile-pic.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* Mobile menu*/}
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigationTabs.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? 'bg-gray-900 text-white'
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
 }
 
 export default Navbar;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    // TODO: add branding colors as default
+    // colors: {
+    //   blue: '#1e11ff',
+    //   pink: '#ff227a',
+    // },
   },
   variants: {
     extend: {},
